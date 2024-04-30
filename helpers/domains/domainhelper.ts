50e6ff168fc4b0106086a6dbab22d635e4b26efc
A Eriksson <79232801+aerksn@users.noreply.github.com>
2024-04-30 17:49:41 +0200

    s

--- helpers/domains/domainhelper.ts
+++ helpers/domains/domainhelper.ts
@@ -34,11 +34,11 @@ export const DomainColumns = [
     label: "URL-längd",
     sortable: true,
   },
-  {
-    key: "available",
-    label: "Tillgänglig",
-    sortable: true,
-  },
+  // {
+  //   key: "available",
+  //   label: "Tillgänglig",
+  //   sortable: true,
+  // },
   // {
   //   key: "cctld",
   //   label: "Landskod",
--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,16 @@
     <div class="max-w-6xl w-full m-auto">
       <h1>Domäner</h1>
       <ClientOnly>
-        <TableViewa></TableViewa>
+        <TableView></TableView>
       </ClientOnly>
     </div>
   </section>
 </template>
 
 <script async setup>
-const TableViewa = defineAsyncComponent(() =>
-  import("~/asyncComponents/TableView.vue")
-);
+// const TableViewa = defineAsyncComponent(() =>
+//   import("~/asyncComponents/TableView.vue")
+// );
 
 const jj = ref(0);
 </script>

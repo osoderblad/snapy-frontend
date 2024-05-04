d011a031efc36b01bdb08c5393a9cb543701f527
A Eriksson <79232801+aerksn@users.noreply.github.com>
2024-05-04 08:03:06 +0200

    dw

--- old/pages/portfolio/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section>
-    <div class="w-full m-auto">
-      <h3>Portfolio</h3>
-    </div>
-  </section>
-</template>
-
-<script setup></script>
--- old/plugins/supabasewrapper.ts
+++
@@ -1,8 +0,0 @@
-// plugins/supabase-wrapper.js
-import { SupabaseWrapper } from "~/utils/SupabaseWrapper";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const client = useSupabaseClient();
-  const wrapper = new SupabaseWrapper(client);
-  nuxtApp.provide("supabaseWrapper", wrapper);
-});

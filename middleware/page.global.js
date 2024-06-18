84a0db9e92f982aefbc64e0d11448a3f25a6e28a
A Eriksson <79232801+aerksn@users.noreply.github.com>
2024-06-19 00:03:25 +0200

    Update page.global.js

--- middleware/page.global.js
+++ middleware/page.global.js
@@ -1,7 +1,14 @@
 export default defineNuxtRouteMiddleware((to) => {
+  if (to.path.includes("/createuser") || to.path.includes("/login")) {
+    const user = useSupabaseUser();
+    // Om användaren inte är inloggad, omdirigera
+    if (user && user.value) {
+      return navigateTo("/");
+    }
+  }
+
   if (to.path.includes("/user")) {
     const user = useSupabaseUser();
-
     // Om användaren inte är inloggad, omdirigera
     if (user && !user.value) {
       return navigateTo("/login");
